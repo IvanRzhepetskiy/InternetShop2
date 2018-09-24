f75f1c209efc8a15e49ce01a8cf5f0c594663831
Ivan <vanyarzepetskiy@gmail.com>
2018-09-24 16:09:26 +0300

    fix for static6

--- InternetShop/settings.py
+++ InternetShop/settings.py
@@ -127,29 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_URL = '/static/'
 #ниже добавил я
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 #added me bellow
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, '/media')
 
 
 #added 24 09
 
 
-
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, "static"),
-)
-
-
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-MEDIA_URL = '/img/'
-
-#MEDIA_ROOT = os.path.join(BASE_DIR, "static", "img")
 
 
 
