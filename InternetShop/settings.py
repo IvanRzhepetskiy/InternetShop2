86efada6157fcd3c6e5d23784521f815df262416
Ivan <vanyarzepetskiy@gmail.com>
2018-09-26 20:40:08 +0300

    form pyCharm

--- InternetShop/settings.py
+++ InternetShop/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-"""
 
+
+"""
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, "static"),
@@ -150,7 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
 
 """
-
 
 
 try:
